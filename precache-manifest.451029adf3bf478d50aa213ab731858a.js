44b1c2ee90dc4ec6e6daab1a03a5b298af11977e
v484407 <manjumala.devim@amerisourcebergen.com>
2020-06-24 18:14:59 +0530

    Updates

---
+++ precache-manifest.451029adf3bf478d50aa213ab731858a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd69c8bb4a0f3524a26dbbeb079b33a",
+    "url": "/Ecomm-complete-frondend/index.html"
+  },
+  {
+    "revision": "4a1f35f7de3e95ad21e5",
+    "url": "/Ecomm-complete-frondend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "247f9bd05ffeab7dc108",
+    "url": "/Ecomm-complete-frondend/static/js/2.08f75dcf.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/Ecomm-complete-frondend/static/js/2.08f75dcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1f35f7de3e95ad21e5",
+    "url": "/Ecomm-complete-frondend/static/js/main.7fc76477.chunk.js"
+  },
+  {
+    "revision": "2ec27e944a748d01fea4",
+    "url": "/Ecomm-complete-frondend/static/js/runtime-main.9d96dfbc.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Ecomm-complete-frondend/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Ecomm-complete-frondend/static/media/shopping-bag.30f07071.svg"
+  }
+]);
